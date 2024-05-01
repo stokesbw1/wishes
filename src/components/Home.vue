899ae2a7bd3f5a83857341518c98de61dfac5cc9
Innocent <ikebosedilekae@gmail.com>
2024-05-01 15:59:08 +0200

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import Nav from "./Nav.vue";
+</script>
+
+<template>
+
+</template>
---
+++ src/components/Nav.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import Vue from 'vue'
+
+</script>
+
+<template>
+    <header>
+        <h1>Wishes</h1>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Book Now</li>
+                <li>Packages</li>
+                <li>Popular Places</li>
+            </ul>
+        </nav>
+        <a href="">Joinn</a>
+    </header>
+</template>
